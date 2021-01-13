e475790ee435997ce909311cdbcf0b3c6f967743
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-13 11:40:04 +0100

    Using opts to control level/state

--- src/get-wd.js
+++ src/get-wd.js
@@ -1,6 +1,13 @@
-module.exports = ({ repo, branch }, lvl = 'root') => {
+module.exports = ({ repo, branch }, opts = {}) => {
+  const defaultOpts = Object.freeze({
+    lvl: 'root',
+    isTmp: true
+  })
+  opts = { ...defaultOpts, ...opts }
+  const { lvl, isTmp } = opts
+  const root = isTmp ? process.env.TMP_ROOT : process.env.ROOT
   const lvlLabels = ['root', 'repo', 'branch']
-  const subPaths = [process.env.ROOT, repo, branch]
+  const subPaths = [root, repo, branch]
   return subPaths
     .slice(0, lvlLabels.indexOf(lvl) + 1)
     .join('/')
