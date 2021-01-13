2b852f7f4866e1ed51d0e62e683e6a24eba36edb
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-13 11:26:09 +0100

    Updated folder structure creation utility

---
+++ src/build-structure.js
@@ -0,0 +1,38 @@
+const { existsSync } = require('fs')
+const exec = require('../utils/exec.js')
+const log = require('../utils/logger.js')
+const getWd = require('./get-wd.js')
+
+module.exports = (repoData, path) => {
+  process.chdir(getWd(repoData)) // goes to root
+  log('Checking folder structure before synchronizing repository data...', 'info')
+  const structureData = checkStructure(path)
+  log('Folder structure successfully checked!', 'success')
+  log('Creating missing folders in structure before synchronizing repository data...', 'info')
+  createFolders(structureData)
+  log('Successfully created all missing folders!', 'success')
+  return structureData
+}
+
+function checkStructure (path) {
+  const folders = path.split('/')
+  return folders
+    .map((folder, i) => {
+      const folderPath = folders
+        .slice(0, i + 1)
+        .join('/')
+      return {
+        path: folderPath,
+        isCreated: existsSync(path)
+      }
+    })
+}
+
+function createFolders (structureData) {
+  structureData.forEach((data, i) => {
+    const { path, isCreated } = data
+    if (i !== structureData.length - 1 && !isCreated) {
+      exec(`mkdir ${path}`)
+    }
+  })
+}
