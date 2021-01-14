fc2db63b79f761a7704b1146081128ecbebcda91
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-14 14:26:03 +0100

    Added comment for watch flag

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
   apps: [{
     name: 'repo-sync-api',
     script: './index.js',
-    watch: false,
+    watch: false, // do not watch since start script handles clean restart
     treekill: false, // do not kill detached child processes
     exec_mode: 'cluster',
     instances: 1
