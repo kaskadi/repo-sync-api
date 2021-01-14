78513219933cd3d5580a882900ae584a79b76eaf
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-14 14:06:55 +0100

    Start app post deployed if start script

--- src/deploy.js
+++ src/deploy.js
@@ -4,11 +4,25 @@ const log = require('../utils/logger.js')
 const cleanup = require('../utils/cleanup.js')
 const buildStructure = require('./build-structure.js')
 
+const { readFileSync, existsSync } = require('fs')
+
 module.exports = (repoData) => {
   const deployDir = getWd(repoData, { lvl: 'branch', isTmp: false })
   const tmpDir = getWd(repoData, { lvl: 'branch' })
   buildStructure(repoData, false)
   log(`Moving all files into deployment folder at ${deployDir}`, 'info')
-  exec(`rsync -avz ${tmpDir}/ ${deployDir}`, true)
-  cleanup([getWd(repoData, { lvl: 'repo' })], true)
+  exec(`rsync -avz ${tmpDir}/ ${deployDir}`)
+  cleanup([getWd(repoData, { lvl: 'repo' })])
+  start(deployDir)
+}
+
+function start (deployDir) {
+  process.chdir(deployDir)
+  if (existsSync('package.json')) {
+    const { scripts } = JSON.parse(readFileSync('package.json', 'utf8'))
+    if (scripts.start) {
+      log('npm start script detected, starting the deployed application...', 'info')
+      exec('npm start', true)
+    }
+  }
 }
