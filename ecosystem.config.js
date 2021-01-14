3c1637ad97bfdf488e209c8d6d5d72a33bda7158
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-14 14:06:45 +0100

    Removed watch flag for pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
   apps: [{
     name: 'repo-sync-api',
     script: './index.js',
-    watch: true,
+    watch: false,
     treekill: false, // do not kill detached child processes
     exec_mode: 'cluster',
     instances: 1
